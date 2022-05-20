3b626f7048fd4d6a5e8608e8245fd74457b49e15
Luana Lessa <lessalsn@gmail.com>
2022-05-20 02:35:46 -0300

    Code review

--- Routes/SignIn.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signIn } from "../Controllers/SignIn.js";
-import signInMiddlware from "../Middleware/SignInMiddleware.js";
-
-const signInRouter = Router();
-
-signInRouter.post('/sign-in', signInMiddlware, signIn);
-
-export default signInRouter;
